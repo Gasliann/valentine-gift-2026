29d6323eac2bfb2a70db3600995d276aaa53befb
Gasliann <josegasparcastrorocha9@gmail.com>
2026-02-01 02:03:35 -0700

    arreglos

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
   // --- TUS DATOS ---
   const eventos = [
     {
-      fecha: "20 Sep 2025",
+      fecha: "23 Nov 2024",
       titulo: "El 'Sí' más importante",
       descripcion: "Hacer click para ver cómo empezó todo...", 
       detalleCompleto: "Tal vez no lo parecia pero en ese momento estaba muuuy nervioso y no encontraba el momento adecuado para preguntarte, Pero cuando me dijiste que sí, fue el mejor momento de mi vida.",
-      imagenes: ["/picnic.jpg", "/picnicFlowers.jpg", "/picnicSelfie.jpg"] 
+      imagenes: ["/picnic.jpeg", "/picnicFlowers.jpeg", "/picnicSelfie.jpeg"] 
     },
     {
       fecha: "23 Dic 2024",
